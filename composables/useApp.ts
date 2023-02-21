25b66e677915894b42b265357469703ec697c6d4
Sam <samuv.eth@gmail.com>
2023-02-21 19:47:48 +0700

    Fix id

--- composables/useApp.ts
+++ composables/useApp.ts
@@ -11,7 +11,10 @@ export function useApp() {
     const ENV = runtimeConfig.public.env;
     let id = "testsnap";
 
-    if (ENV !== "develop" && window.location.hostname.includes("snapshoot")) {
+    if (
+      ENV !== "develop" &&
+      window.location.hostname.split(".")[1] === "snapshoot"
+    ) {
       id = window.location.hostname.split(".")[0];
     }
 

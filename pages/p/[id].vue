12e1e5c178e66b0599ed0f4cb1843bd78972e3a9
Sam <samuv.eth@gmail.com>
2023-02-22 17:38:08 +0700

    Add meta

--- pages/p/[id].vue
+++ pages/p/[id].vue
@@ -1,15 +1,27 @@
 <script setup lang="ts">
 import { PROPOSAL_QUERY } from "@/helpers/queries";
+import { Proposal } from "@/helpers/interfaces";
 
 const route = useRoute();
 
-const { data } = await useAsyncQuery(PROPOSAL_QUERY, {
+const { data } = await useAsyncQuery<{
+  proposal: Proposal;
+}>(PROPOSAL_QUERY, {
   id: route.params.id,
+});
+
+const proposal = computed(() => data.value?.proposal);
+
+useSeoMeta({
+  title: () => proposal.value?.title ?? "",
+  description: () => proposal.value?.body ?? "",
+  ogTitle: () => proposal.value?.title ?? "",
+  ogDescription: () => proposal.value?.body ?? "",
 });
 </script>
 
 <template>
   <BaseContainer>
-    <div>{{ data }}</div>
+    <div>{{ proposal }}</div>
   </BaseContainer>
 </template>
